0c4b37a52535666e57711f0ee5e50c3b2f3d34b5
Regina Cupa <reginacupa@gmail.com>
2024-05-21 19:15:56 -0300

    adicionando os primeiros codigos  JavaScript

---
+++ js/script.js
@@ -0,0 +1,48 @@
+//ativar links do menu
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+    if (url.includes (href)) {
+        link.classList.add('ativo');
+    }
+}
+links.forEach (ativarLink);
+
+//ativar itens do orçamento
+
+const paramentros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }
+
+    console.log(elemento);
+}
+
+paramentros.forEach (ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute("aria-controls");
+    const resposta = document.getElementById(controls);
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute("aria-expanded", ativa);
+    console.log(ativa);
+
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener ("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
